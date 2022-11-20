6d4cba2584eae44c198a7bc17c538a39e281862f
LoremAmoris <kirillgukov@mail.ru>
2022-11-20 13:43:13 +0300

    HomeWork_1

--- hw_1/tests/test_3.py
+++ hw_1/tests/test_3.py
@@ -5,7 +5,7 @@ from hw_1.tasks.task_3 import find_maximum_and_minimum
 @pytest.mark.parametrize(
     ["value", "expected_result"],
     [
-        ("file_name.txt", [1, 5]),
+        ('file_name.txt', [1, 6]),
     ],
 )
 def test_find_maximum_and_minimum(value: str, expected_result: tuple[int, int]):
