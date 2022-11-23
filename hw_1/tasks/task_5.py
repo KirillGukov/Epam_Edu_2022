60e8894907b083f35b0626af331f03b2fb980f80
LoremAmoris <kirillgukov@mail.ru>
2022-11-23 14:51:13 +0300

    HomeWork_1

--- hw_1/tasks/task_5.py
+++ hw_1/tasks/task_5.py
@@ -3,15 +3,12 @@ from typing import List
 
 def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
     num_sum_start = sum(nums[0:k])
+    num_sum_end = sum(nums[1:(k + 1)])
     i = 0
-    count = 0
-    num_sum_end = 0
     while k + i < len(nums):
         num_sum_end = sum(nums[(i+1):(k+i+1)])
-        count += 1
         i += 1
         if num_sum_start >= num_sum_end:
             num_sum_end = num_sum_start
     nums_sum = num_sum_end
     return nums_sum
-
