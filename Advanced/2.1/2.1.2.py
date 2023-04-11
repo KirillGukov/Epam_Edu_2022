7d288899c1826ebc0137dac54f59e54e1ef2ef05
LoremAmoris <kirillgukov@mail.ru>
2023-04-11 13:42:18 +0300

    LoremAmoris

--- Advanced/2.1/2.1.2.py
+++
@@ -1,7 +0,0 @@
-a, b = float(input()), float(input())
-if a / (b**2) < 18.5:
-    print('Недостаточная масса')
-elif a / (b**2) > 25:
-    print('Избыточная масса')
-else:
-    print('Оптимальная масса')
